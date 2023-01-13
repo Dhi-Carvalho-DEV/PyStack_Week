1ef854bece367b2ac312a850373beb58c23625db
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-01-13 09:02:44 -0300

    FInalização de estruturas de repetição

--- curso_introdutorio/estruturasDeRepeticao.py
+++ curso_introdutorio/estruturasDeRepeticao.py
@@ -10,7 +10,10 @@ while i < 10:
 aluno = 'S'
 while aluno != 'N':
     nome = input('Digite o nome do aluno: ')
-    media = float(input('Digite a média de ' + nome + ': '))
+    nota1 = float(input('Digite a primeira nota de ' + nome + ': '))
+    nota2 = float(input('Digite a primeira nota de ' + nome + ': '))
+
+    media = (nota1 + nota2) / 2
 
     if media >= 6:
         print(f'O {nome} foi aprovado.')
@@ -21,3 +24,20 @@ while aluno != 'N':
 
 print('FOR')
 
+for i in range(0, 10):
+    print(i)
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(f'O número {i} é par')
+    else:
+        print(f'O número {i} é ímpar')
+
+for i in range(1, 11):
+    print(f'======Tabuada do {i}======')
+    for j in range(1, 11):
+        print(f'{i} X {j} == {i*j}')
+
+tabuada = int(input('Digite a tabuada desejada: '))
+for i in range(1, 11):
+    print(f'{tabuada} X {i} == {i*tabuada}')
